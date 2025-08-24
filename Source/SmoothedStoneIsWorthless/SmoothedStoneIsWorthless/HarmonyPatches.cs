5fa06090fc114d22a620439e69433352b09ef800
KonradHeinser <robhos2000@gmail.com>
2025-08-24 12:14:52 -0700

    Floor patch

---
+++ Source/SmoothedStoneIsWorthless/SmoothedStoneIsWorthless/HarmonyPatches.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace SmoothedStoneIsWorthless
+{
+    [StaticConstructorOnStartup]
+    public static class HarmonyPatches
+    {
+        private static readonly Type patchType = typeof(HarmonyPatches);
+        static HarmonyPatches()
+        {
+            Harmony harmony = new Harmony("Rimworld.Alite.SSiW.main");
+
+            harmony.Patch(AccessTools.Method(typeof(TerrainDefGenerator_Stone), nameof(TerrainDefGenerator_Stone.ImpliedTerrainDefs)),
+                postfix: new HarmonyMethod(patchType, nameof(ImpliedTerrainDefsPostfix)));
+        }
+
+        public static void ImpliedTerrainDefsPostfix(ref IEnumerable<TerrainDef> __result)
+        {
+            var result = new List<TerrainDef>(__result);
+            foreach (var terrainDef in result)
+                if (terrainDef.defName.EndsWith("_Smooth"))
+                    StatUtility.SetStatValueInList(ref terrainDef.statBases, StatDefOf.MarketValue, 0f);
+            __result = result;
+        }
+    }
+}
